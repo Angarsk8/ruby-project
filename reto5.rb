e7fab1543a8c7d2de9eb697cdfa20f2d1d1f202a
Andrés García H <andresa.garciah621@gmail.com>
2015-06-27 20:41:01 -0500

    add the logic to the program in order to mimic the behaviour shown in the README file

--- reto5.rb
+++ reto5.rb
@@ -3,3 +3,15 @@ require 'csv'
 definitions = CSV.read('definitions').shuffle
 
 puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
+
+definitions.each do |value|
+    puts "Definición", "#{value[0]}:"
+    loop do
+        print "Adivinar: "
+        answer = gets.chomp.strip.downcase
+        valid = answer == value[1].downcase
+        puts valid ? "Correcto" : "Incorrecto!, Trata de nuevo"
+        break if valid
+    end
+end
+    
