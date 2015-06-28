e5c43a4e84abdff07657dfaa4e2e80bbd8dbc7b7
Andrés García H <andresa.garciah621@gmail.com>
2015-06-27 20:42:00 -0500

    Fix a typo

--- reto5.rb
+++ reto5.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-definitions = CSV.read('definitions').shuffle
+definitions = CSV.read('definitions.txt').shuffle
 
 puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
 
