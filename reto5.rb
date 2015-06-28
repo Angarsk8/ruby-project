52394ce838d69d278aa5c6f8af6fc5b27a68e4e7
Andrés García H <andresa.garciah621@gmail.com>
2015-06-27 20:34:17 -0500

    Use the csv library to load the definitions in the definitions.txt file into an array

---
+++ reto5.rb
@@ -0,0 +1,5 @@
+require 'csv'
+
+definitions = CSV.read('definitions').shuffle
+
+puts "Bienvenido a reto 5, Para jugar, solo ingresa el termino correcto para cada una de las definiciones, Listo? Vamos!"
