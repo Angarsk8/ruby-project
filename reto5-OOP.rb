64b3270b30abee39245c67395edab55f0cd1dd72
Andrés García H <andresa.garciah621@gmail.com>
2015-07-03 05:03:59 -0500

    import the csv library and create the skeleton of the class

---
+++ reto5-OOP.rb
@@ -0,0 +1,30 @@
+require 'csv'
+
+class Reto5
+    
+    def initialize(text_file_name)
+    end
+    
+    def start
+    end
+    
+    def load_definitions(text_file_name)
+    end
+    
+    def greet
+    end
+    
+    def play(list_of_definitions)
+    end
+    
+    def congratulate
+    end
+    
+    def get_definitions
+    end
+    
+    def get_last_attempts
+    end
+    
+    private :load_definitions, :greet, :play, :congratulate
+end
